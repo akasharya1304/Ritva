4c95705e8bcf5a0fdf42144981ffa87254795622
Akashsmilebots <akash@smilebots.com>
2026-04-26 23:27:50 +0530

    small fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone', 
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./generated/prisma/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
